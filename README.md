683ece41d9484353ef9b2ed30363898e543d2dea
S.P. Mohanty <spmohanty91@gmail.com>
2015-12-31 11:59:50 +0530

    Add kathha-client as a submodule, and setup static file serving using express

---
+++ README.md
@@ -0,0 +1,7 @@
+KATHAA : A Visual Programming Framework for Sampark Machine Translation framework
+=================================================================================
+
+
+Author
+======
+S.P. Mohanty <spmohanty91@gmail.com>
--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var io = require('socket.io')();
+var express = require('express');
 
+//Express Server defitnition
+var app = express();
+app.use(express.static(__dirname+"/kathaa"))
+app.listen(8000);
+
+
+//Socket.IO Definitions
 io.of('/kathaa')
   .on('connection', function(socket){
     console.log("Connected !!");
-    socket.on('command', function (command) {
-      console.log(command);
-      socket.emit('command_ack', { id: 'random_id' });    
+    socket.on('request', function (client) {
+      console.log(client);
+      var request_id = "request_"+random_string();
+      client.emit('request_ack', { response: "acknowledged", id: request_id });
+
     });
   })
-
 io.listen(8009);
+
+//Helper functions
+function random_string(){
+  return Math.random().toString(36).substring(7);
+}
