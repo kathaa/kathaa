3f1d6211642c91323b1dababdd6537e6969d9805
S.P. Mohanty <spmohanty91@gmail.com>
2015-12-31 17:22:20 +0530

    added module library defitnion to backend

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var io = require('socket.io')();
 var express = require('express');
+var jsonfile = require('jsonfile');
+var util = require('util');
+
+//Load Module Libraries
+var file = __dirname+"/module_library/package.json";
+var module_package = jsonfile.readFileSync(file);
+var module_library = {}
+module_package.files.forEach(function(element, index, array){
+  console.log(element);
+  jsonConcat(module_library, jsonfile.readFileSync(__dirname+"/module_library/"+element));
+})
 
 //Express Server defitnition
 var app = express();
@@ -11,11 +22,13 @@ app.listen(8000);
 io.of('/kathaa')
   .on('connection', function(socket){
     console.log("Connected !!");
-    socket.on('request', function (client) {
-      console.log(client);
-      var request_id = "request_"+random_string();
-      client.emit('request_ack', { response: "acknowledged", id: request_id });
+    //Instantiate Client
+    socket.emit('init', {module_library: module_library});    
 
+    socket.on('request', function (client) {
+      // console.log(client);
+      // var request_id = "request_"+random_string();
+      // client.emit('request_ack', { response: "acknowledged", id: request_id });
     });
   })
 io.listen(8009);
@@ -23,4 +36,11 @@ io.listen(8009);
 //Helper functions
 function random_string(){
   return Math.random().toString(36).substring(7);
+}
+
+function jsonConcat(o1, o2) {
+ for (var key in o2) {
+  o1[key] = o2[key];
+ }
+ return o1;
 }
